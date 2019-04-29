40da930bab1dfc30dbef146fe7235f62ca2be5e1
jonathancote84 <jonathancote92@gmail.com>
2019-04-29 08:11:04 -0700

    Tabs content added

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
-
+import React from "react";
+import Tabs from './state/Tabs';
 import './App.css';
-// import Bomb from './state-drills/Bomb';
-import RouletteGun from './state-drills/RouletteGun';
 
 
 
 
+//array of the objects each with a name and content
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
 
+];
 
-function App(){
-    return (
-    <main className='App'>
-      <RouletteGun />
-    </main>
-  )
+class App extends React.Component{
+  render() {
+    return <div>
+      <Tabs tabs={tabsProp} />
+    </div>
+    
+   
+    
+  }
+
 }
 
 export default App;
